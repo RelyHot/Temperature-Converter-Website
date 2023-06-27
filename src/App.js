072d9593725a686290a73073f96d4f08740ab188
RelyHot <aroramayank829@gmail.com>
2023-06-27 17:21:43 +0530

    Added Bootstrap Library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import TemperatureConverter from './TemperatureConverter';
+import Navbar from './Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <TemperatureConverter />
     </div>
   );
